7d9e1253d9848f8b47054835e9a7b808b9e0d165
qw050123 <qw050123@gmail.com>
2023-04-27 16:56:52 +0800

    add: extention prettier format

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,24 +7,25 @@
         </v-btn>
       </NuxtLink>
       <v-col class="text-center mt-4 text-blue-grey-darken-4" cols="12">
-        {{ new Date().getFullYear() }} — <strong>HexSchool 北三組 專題使用</strong>
+        {{ new Date().getFullYear() }} —
+        <strong>HexSchool 北三組 專題使用</strong>
       </v-col>
     </v-row>
   </v-footer>
 </template>
 
 <script setup>
-const links = ref([
-  { title: '關於我們', to: '/about' },
-  { title: '使用者條款', to: '/terms' },
-  { title: '隱私權政策', to: '/privacy' },
-  { title: '常見問題', to: '/faq' }
-])
+  const links = ref([
+    { title: "關於我們", to: "/about" },
+    { title: "使用者條款", to: "/terms" },
+    { title: "隱私權政策", to: "/privacy" },
+    { title: "常見問題", to: "/faq" },
+  ]);
 </script>
 
 <style lang="scss" scoped>
-.footer {
-  max-height: 180px;
-  transform: translateY(100%);
-}
+  .footer {
+    max-height: 180px;
+    transform: translateY(100%);
+  }
 </style>
