a44af5cc88b8b9b615017598eb6a5001096cac80
rere2133 <rere2133@users.noreply.github.com>
2023-04-27 12:38:23 +0800

    Add: link paths

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
   <v-footer class="bg-grey-lighten-3 footer">
     <v-row justify="center" no-gutters>
-      <v-btn v-for="link in links" :key="link" variant="text" class="mx-2 text-blue-grey-darken-1" rounded="xl">
-        {{ link }}
-      </v-btn>
+      <NuxtLink v-for="link in links" :key="link" :to="link.to">
+        <v-btn variant="text" class="mx-2 text-blue-grey-darken-1" rounded="xl">
+          {{ link.title }}
+        </v-btn>
+      </NuxtLink>
       <v-col class="text-center mt-4 text-blue-grey-darken-4" cols="12">
         {{ new Date().getFullYear() }} — <strong>HexSchool 北三組 專題使用</strong>
       </v-col>
@@ -13,10 +15,10 @@
 
 <script setup>
 const links = ref([
-  '關於我們',
-  '使用者條款',
-  '隱私權政策',
-  '常見問題',
+  { title: '關於我們', to: '/about' },
+  { title: '使用者條款', to: '/terms' },
+  { title: '隱私權政策', to: '/privacy' },
+  { title: '常見問題', to: '/faq' }
 ])
 </script>
 
