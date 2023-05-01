7d4a40a37e09f29584dfbad7891f82e3cbf213af
annaliu2021 <annaprosty@gmail.com>
2023-05-01 22:53:31 +0800

    Add: env

--- services/apis.js
+++ services/apis.js
@@ -1,6 +1,5 @@
-const BACKEND_ROOT_PROD = import.meta.env.BACKEND_ROOT_PROD;
-const BACKEND_ROOT_DEV = import.meta.env.BACKEND_ROOT_DEV;
-const BACKEND_ROOT = BACKEND_ROOT_DEV
+const { VITE_BACKEND_ROOT_DEV, VITE_BACKEND_ROOT_PROD } = import.meta.env;
+const BACKEND_ROOT = VITE_BACKEND_ROOT_DEV
 
 //會員帳號 - 取得會員資料
 export const getAccountInfo = `${BACKEND_ROOT}/account/info-form?uid=644f06e37ca2aa24ddb66e7e`;
