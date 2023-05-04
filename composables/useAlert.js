839a85c0bda0e8a77b9ca62c8defe38dc0374bef
AnnannannA <83194360+annaliu2021@users.noreply.github.com>
2023-05-04 10:32:59 +0800

    Add: deleteComfirmbox()

--- composables/useAlert.js
+++ composables/useAlert.js
@@ -3,8 +3,23 @@ import "sweetalert2/dist/sweetalert2.min.css";
 import "@/assets/css/swal.css";
 
 export const useAlert = () => {
+  //基本視窗(要按OK才會關閉)
+  const basicBox = function (text, nuxtFunc) {
+    swal.fire({ title: "", text, confirmButtonText: "確定" }).then((result) => {
+      if (result.isConfirmed && nuxtFunc && typeof nuxtFunc === "function") {
+        nuxtFunc();
+      }
+    });
+    document
+      .querySelector(".swal2-container.swal2-center.swal2-backdrop-show")
+      .classList.add("sp-basicBox");
+    document.querySelector(
+      ".swal2-styled.swal2-confirm"
+    ).style.backgroundColor = "#fff";
+  };
+
   //確認視窗
-  const confirmBox = function (text, successFunc) {
+  const confirmBox = function (text, agreeFunc) {
     swal
       .fire({
         title: "",
@@ -17,8 +32,8 @@ export const useAlert = () => {
       .then((result) => {
         // console.log(result, "result");
         if (result.isConfirmed) {
-          if (typeof successFunc === "function") {
-            successFunc();
+          if (typeof agreeFunc === "function") {
+            agreeFunc();
           }
         }
       });
@@ -32,31 +47,39 @@ export const useAlert = () => {
     ).style.backgroundColor = "#fff";
   };
 
-  //基本視窗(要按OK才會關閉)
-  const basicBox = function (text, nuxtFunc) {
-    if (process.client) {
-      swal
-        .fire({ title: "", text, confirmButtonText: "確定" })
-        .then((result) => {
-          if (
-            result.isConfirmed &&
-            nuxtFunc &&
-            typeof nuxtFunc === "function"
-          ) {
-            nuxtFunc();
+  //刪除確認視窗
+  const deleteConfirmBox = function (text, agreeFunc) {
+    swal
+      .fire({
+        title: "刪除確認",
+        text,
+        showDenyButton: false,
+        showCancelButton: true,
+        confirmButtonText: "確定",
+        cancelButtonText: "取消",
+      })
+      .then((result) => {
+        // console.log(result, "result");
+        if (result.isConfirmed) {
+          if (typeof agreeFunc === "function") {
+            agreeFunc();
           }
-        });
-      document
-        .querySelector(".swal2-container.swal2-center.swal2-backdrop-show")
-        .classList.add("sp-basicBox");
-      document.querySelector(
-        ".swal2-styled.swal2-confirm"
-      ).style.backgroundColor = "#fff";
-    }
+        }
+      });
+    document
+      .querySelector(".swal2-container.swal2-center.swal2-backdrop-show")
+      .classList.add("sp-deleteConfirmBox");
+    document.querySelector(".swal2-styled.swal2-cancel").style.backgroundColor =
+      "#fff";
+    document.querySelector(
+      ".swal2-styled.swal2-confirm"
+    ).style.backgroundColor = "#fff";
+    document.querySelector(".swal2-title").style.color = "#fff";
   };
 
   return {
-    confirmBox,
     basicBox,
+    confirmBox,
+    deleteConfirmBox,
   };
 };
