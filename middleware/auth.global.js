8e29c546d7cc4841021c6875ae5a7ca47315d83e
AnnannannA <83194360+annaliu2021@users.noreply.github.com>
2023-05-10 14:52:35 +0800

    Fix: bug

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,15 +2,16 @@ import { siteConfig } from "@/services/siteConfig";
 
 export default defineNuxtRouteMiddleware((to, from) => {
   if (
-    to.path.startsWith("/account") &&
-    useCookie("spToken").value.length == 0
+    to.path.startsWith(siteConfig.linkPaths.account.to) &&
+    //useCookie("spToken").value.length == 0 : (useCookie("spToken").value) maybe undefined
+    !useCookie("spToken").value
   ) {
-    return navigateTo("/");
+    return navigateTo(siteConfig.linkPaths.home.to);
   }
 
   if (
     to.path.startsWith(siteConfig.linkPaths.postTask.to) &&
-    useCookie("spToken").value.length == 0
+    !useCookie("spToken").value
   ) {
     return navigateTo(siteConfig.linkPaths.login.to);
   }
