1848d1f2a21861da988a07477b4d3d8c43a3f748
AnnannannA <83194360+annaliu2021@users.noreply.github.com>
2023-05-10 11:35:39 +0800

    Add: get Profile Status

--- services/apis/account.js
+++ services/apis/account.js
@@ -1,3 +1,5 @@
+const { req } = useHttp();
+
 //會員帳號 - 取得會員資料
 //export const getAccountInfoUrl = `/account/info-form?uid=645489d1bfd4b610ce7a6962`;
 export const getAccountInfoUrl = `/account/info-form`;
@@ -5,3 +7,7 @@ export const getAccountInfoUrl = `/account/info-form`;
 //會員帳號 - 修改會員資料
 //export const updateAccountInfoUrl = `/account/info-form?uid=645489d1bfd4b610ce7a6962`;
 export const patchAccountInfoUrl = `/account/info-form`;
+
+//取得點數狀態
+export const getProfileStatus = () =>
+  req("GET", "/account/profile-stats", {}, { auth: true });
