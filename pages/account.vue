b587a811170e976765a434082cf63fbed6563fd0
qw050123 <qw050123@gmail.com>
2023-05-11 14:20:52 +0800

    refactor: account layout

--- pages/account.vue
+++ pages/account.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <h1>會員</h1>
-    <div class="sp-flex sp-gap-4 sp-my-4">
-      <NuxtLink to="/account"><v-btn>我的帳號</v-btn></NuxtLink>
-      <NuxtLink to="/account/tasks"><v-btn>任務管理</v-btn></NuxtLink>
-      <NuxtLink to="/account/comments"><v-btn>評價查看</v-btn></NuxtLink>
-      <NuxtLink to="/account/points"><v-btn>點數管理</v-btn></NuxtLink>
-    </div>
+    <VContainer fluid>
+      <VRow>
+        <VCol>
+          <h1>會員</h1>
+        </VCol>
+      </VRow>
+    </VContainer>
+
     <NuxtPage />
   </div>
 </template>
 
 <script setup>
-
+  definePageMeta({
+    layout: "account",
+  });
 </script>
 
 <style lang="scss" scoped></style>
