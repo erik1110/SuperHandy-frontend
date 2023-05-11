9d3675f60f2bae865df97c32bbc40a891dc817be
qw050123 <qw050123@gmail.com>
2023-05-11 14:21:21 +0800

    Fixed: tab value

--- pages/find-tasks/[view].vue
+++ pages/find-tasks/[view].vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <h1>找任務</h1>
-    <v-tabs color="secondary" class="my-5">
-      <NuxtLink to="/find-tasks/list"><v-tab>列表模式</v-tab></NuxtLink>
-      <NuxtLink to="/find-tasks/map"><v-tab>地圖模式</v-tab></NuxtLink>
+    <v-tabs color="secondary" class="my-5" v-model="view">
+      <NuxtLink to="/find-tasks/list"
+        ><v-tab value="list">列表模式</v-tab></NuxtLink
+      >
+      <NuxtLink to="/find-tasks/map"
+        ><v-tab value="map">地圖模式</v-tab></NuxtLink
+      >
     </v-tabs>
     <h2 v-if="view === 'list'">列表模式</h2>
     <h2 v-if="view === 'map'">地圖模式</h2>
@@ -11,13 +15,13 @@
 </template>
 
 <script setup>
-const route = useRoute()
-const view = route.params.view
-onMounted(() => {
-  if (view !== 'list' && view !== 'map') {
-    navigateTo('/find-tasks/list')
-  }
-})
+  const route = useRoute();
+  const view = route.params.view;
+  onMounted(() => {
+    if (view !== "list" && view !== "map") {
+      navigateTo("/find-tasks/list");
+    }
+  });
 </script>
 
 <style lang="scss" scoped></style>
