ab23bcbb8bd6a2ee0505849c1e09238ac2bc9b81
rere2133 <rere2133@users.noreply.github.com>
2023-05-11 12:28:59 +0800

    Add: sec 4 completed case

---
+++ components/Home/CompletedCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="caseCard">
+    <v-card min-width="300" elevation="0">
+      <div class="sp-flex sp-justify-between sp-items-center">
+        <div class="left sp-basis-2/3">
+          <!-- title -->
+          <div class="sp-flex sp-items-center sp-mb-2">
+            <p class="sp-text-h5">
+              {{ title }}
+            </p>
+            <div class="successTag">已完成</div>
+
+          </div>
+          <!-- time & location -->
+          <div class="sp-flex">
+
+            <p class="sp-inline-flex sp-whitespace-nowrap">
+              <ClockIcon class="sp-w-6 sp-h-6 sp-text-purple" />{{ timeFormat(completedAt) }}
+            </p>
+
+            <p class="sp-inline-flex sp-whitespace-nowrap sp-ml-4 hover:sp-text-purple hover:sp-cursor-pointer">
+              <MapPinIcon class="sp-w-6 sp-h-6 sp-text-purple" /> {{ location.landmark }}
+            </p>
+          </div>
+        </div>
+        <div class="right">
+          <span class="sp-text-h3 sp-whitespace-nowrap">
+            ${{ salary }} <span class="sp-text-body"> /次</span>
+          </span>
+        </div>
+      </div>
+
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+import IconTime from "@/assets/images/icons/time.svg"
+import IconLocation from "@/assets/images/icons/location.svg"
+import { ClockIcon, MapPinIcon } from '@heroicons/vue/24/solid'
+const { cardData } = defineProps({
+  cardData: Object,
+})
+const { location, title, salary, completedAt } = cardData
+// Moment
+const { timeFormat } = useMoment()
+</script>
+
+<style lang="postcss" scoped>
+.caseCard {
+  @apply sp-mx-auto sp-w-[90%] sp-p-6 sp-max-w-lg sp-min-w-[420px] sp-mb-4 sp-rounded-2xl sp-shadow-lg
+}
+
+.successTag {
+  @apply sp-inline-flex sp-items-center sp-rounded-md sp-bg-gray-100 sp-px-4 sp-py-2 sp-text-xs sp-font-medium sp-ml-2
+}
+</style>
--- components/Home/Steper.vue
+++ components/Home/Steper.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="sp-flex sp-items-center">
-    <div class="circle"></div>
+    <div class="circle flexCenter">
+      <Component :is="icons[icon]" class="sp-w-12 sp-h-12 sp-text-primary" />
+    </div>
     <div class="sp-whitespace-nowrap">
-      <h3 class="sp-text-h3">
+      <h3 class="sp-text-h3 sp-mb-2">
         {{ title }}
       </h3>
       <p class="sp-text-title">{{ subtitle }}</p>
@@ -12,14 +14,23 @@
 </template>
 
 <script setup>
+import { NewspaperIcon, UserGroupIcon, HandThumbUpIcon } from '@heroicons/vue/24/solid'
 const props = defineProps({
   stepData: Object
 });
 const { icon, title, subtitle } = props.stepData
+// Icons
+const icons = reactive({
+  NewspaperIcon,
+  UserGroupIcon,
+  HandThumbUpIcon
+})
+
+
 </script>
 
 <style lang="postcss" scoped>
 .circle {
-  @apply sp-min-w-[100px] sp-min-h-[100px] sp-bg-primary-lighten sp-rounded-full sp-mr-4
+  @apply sp-min-w-[100px] sp-min-h-[100px] sp-bg-primary-lighten sp-rounded-full sp-mr-8
 }
 </style>
