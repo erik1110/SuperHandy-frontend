ea8da12a95e99257816131de6c96cd779c285465
qw050123 <qw050123@gmail.com>
2023-05-13 21:20:30 +0800

    Add: point history

--- services/apis/point.js
+++ services/apis/point.js
@@ -3,5 +3,8 @@ const { req } = useHttp();
 export const getAccountPoints = () =>
   req("GET", `/account/points`, {}, { auth: true });
 
+export const getAccountPointsHistory = () =>
+  req("GET", `/account/points/history`, {}, { auth: true });
+
 export const postPointspurchase = (data) =>
   req("POST", `/account/points/purchase`, data, { auth: true });
