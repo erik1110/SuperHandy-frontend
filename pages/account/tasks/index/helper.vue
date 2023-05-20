498aadcd79292e618e0b4fe3bf952ba2e3ed9496
qw050123 <qw050123@gmail.com>
2023-05-20 18:39:46 +0800

    Add: tasks list

---
+++ pages/account/tasks/index/helper.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <v-tabs v-model="groupTab" color="secondary">
+      <v-tab value="all">全部 [{{ postList.all.length }}]</v-tab>
+      <v-tab value="published">媒合中 [{{ postList.published.length }}]</v-tab>
+      <v-tab value="inProgressed"
+        >進行中 [{{ postList.inProgressed.length }}]</v-tab
+      >
+      <v-tab value="confirmed">已完成 [{{ postList.confirmed.length }}]</v-tab>
+      <v-tab value="unpublished"
+        >已下架 [{{ postList.unpublished.length }}]</v-tab
+      >
+      <v-tab value="other">未成立 [{{ postList.other.length }}]</v-tab>
+    </v-tabs>
+    <v-card class="sp-mb-4" v-for="(item, index) in postList[groupTab]">
+      <v-card-text>
+        <h4 class="sp-text-h4 sp-inline-block sp-mb-2">{{ item.title }}</h4>
+        <div
+          class="sp-w-[94.25px] sp--translate-y-2 sp-px-4 sp-py-2 sp-text-center sp-border-2 sp-border-black sp-border-solid sp-float-right"
+        >
+          {{ item.status }}
+        </div>
+        <div class="sp-text-body">地點：{{ item.address }}</div>
+        <div class="sp-text-body">薪水：${{ item.salary }}</div>
+        <span class="sp-text-body">
+          刊登：{{ new Date(item.publishedAt).toLocaleString() }}
+          <span class="sp-color-red-0 sp-text-red-800"
+            >(${{ new Date(item.expiredAt).toLocaleString() }}
+            過期)
+          </span>
+        </span>
+        <v-btn
+          color="primary"
+          outlined
+          class="sp--translate-y-2 sp-px-4 sp-py-2 sp-text-center sp-border-2 sp-border-black sp-border-solid sp-float-right"
+        >
+          {{ item.status == "草稿" ? "編輯草稿" : "查看詳情" }}
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+<script setup>
+  import { getTasksHelperManagement } from "@/services/apis/tasks";
+  const groupTab = useState("all");
+  const postList = ref({
+    all: [],
+    published: [],
+    inProgressed: [],
+    confirmed: [],
+    unpublished: [],
+    other: [],
+  });
+  const listModel = {
+    all: [],
+    published: [],
+    inProgressed: [],
+    confirmed: [],
+    unpublished: [],
+    other: [],
+  };
+  let FuncGetTasksHelperManagement = async () => {
+    let res = await getTasksHelperManagement();
+    if (!res.error) {
+      let resArray = res.data;
+      postList.value = listModel;
+      resArray.forEach(function (item) {
+        postList.value.all.push(item);
+        switch (item.status) {
+          case "媒合中": {
+            postList.value.published.push(item);
+            break;
+          }
+          case "進行中": {
+            postList.value.inProgressed.push(item);
+            break;
+          }
+          case "已完成": {
+            postList.value.confirmed.push(item);
+            break;
+          }
+          case "已下架": {
+            postList.value.unpublished.push(item);
+            break;
+          }
+          default: {
+            postList.value.other.push(item);
+            break;
+          }
+        }
+      });
+    }
+  };
+  onMounted(() => {
+    FuncGetTasksHelperManagement();
+  });
+</script>
+<style></style>
--- services/apis/postTask.js
+++ services/apis/postTask.js
@@ -1,27 +1,45 @@
 const { req } = useHttp();
 
 // 檢查地址
-export const getLocation = (data) => req("GET", "/post-task/check-location",data,{ auth: true });
+export const getLocation = (data) =>
+  req("GET", "/post-task/check-location", data, { auth: true });
 
 // 儲存為草稿
-export const postDraft = (data) => req("POST", "/post-task/draft",data,{ auth: true });
+export const postDraft = (data) =>
+  req("POST", "/post-task/draft", data, { auth: true });
 
 // 直接刊登任務
-export const postPublish = (data) => req("POST", "/post-task/publish",data,{ auth: true });
+export const postPublish = (data) =>
+  req("POST", "/post-task/publish", data, { auth: true });
 
 // 從草稿刊登任務
-export const postPublishFromDraft = (taskId,data) => req("POST", `/post-task/draft/${taskId}`,data,{ auth: true });
+export const postPublishFromDraft = (taskId, data) =>
+  req("POST", `/post-task/draft/${taskId}`, data, { auth: true });
 
 // 取得草稿byTaskId
-export const getDraftById = (taskId) => req("GET", `/post-task/draft/${taskId}`,{},{ auth: true });
+export const getDraftById = (taskId) =>
+  req("GET", `/post-task/draft/${taskId}`, {}, { auth: true });
 
 // 刪除草稿byTaskId
-export const deleteDraftById = (taskId) => req("DELETE", `/post-task/draft/${taskId}`,{},{ auth: true });
+export const deleteDraftById = (taskId) =>
+  req("DELETE", `/post-task/draft/${taskId}`, {}, { auth: true });
 
 // 更新草稿byTaskId
-export const putDraftById = (taskId,data) => req("PUT", `/post-task/draft/${taskId}`,data,{ auth: true });
+export const putDraftById = (taskId, data) =>
+  req("PUT", `/post-task/draft/${taskId}`, data, { auth: true });
 
+//下架任務
+export const postPostTaskUnpublish = (taskId) =>
+  req("POST", `/post-task/unpublish/${taskId}`, {}, { auth: true });
 
+//上架任務
+export const postPostTaskRepublish = (taskId) =>
+  req("POST", `/post-task/republish/${taskId}`, {}, { auth: true });
+
+//編輯下嫁任務
+export const postPostTaskEdit = (taskId, data) =>
+  req("POST", `/post-task/edit/${taskId}`, data, { auth: true });
 
 // 取得已發布的任務
-export const getTasksById = (taskId,data) => req("GET", `/tasks/management/${taskId}`,data,{ auth: true });
+export const getTasksById = (taskId, data) =>
+  req("GET", `/tasks/management/${taskId}`, data, { auth: true });
