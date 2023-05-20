3d5e3a2cde6b4960068569113a81cf5aa740917d
rere2133 <rere2133@users.noreply.github.com>
2023-05-20 18:04:21 +0800

    Add: filters form

--- components/FindTasks/Filters.vue
+++ components/FindTasks/Filters.vue
@@ -6,69 +6,112 @@
     </div>
     <!-- 排序 -->
     <div class="wrap sp-border-b sp-border-slate-200">
-      <p class="sp-min-w-[40px] sp-text-gray-placeholder sp-font-bold">排序</p>
+      <p
+        class="sp-min-w-[36px] sp-text-body-sm sp-text-gray-placeholder sp-font-bold"
+      >
+        排序
+      </p>
       <v-radio-group
-        v-model="sort"
+        v-model="filterData.sort"
         color="v-purple"
         inline
         hide-details
         density="compact"
       >
-        <template #prepend> </template>
-        <v-radio label="最新刊登" value="latest"></v-radio>
-        <v-radio label="最高薪水" value="salary"></v-radio>
-        <v-radio label="最多詢問" value="query"></v-radio>
+        <v-radio value="latest" class="mr-1">
+          <template #label>
+            <span class="sp-text-body-sm">最新刊登 </span>
+          </template>
+        </v-radio>
+        <v-radio value="salary" class="mr-1">
+          <template #label>
+            <span class="sp-text-body-sm">最高薪水 </span>
+          </template>
+        </v-radio>
+        <v-radio value="query" class="mr-1">
+          <template #label>
+            <span class="sp-text-body-sm">最多詢問 </span>
+          </template>
+        </v-radio>
       </v-radio-group>
     </div>
     <!-- 進階 -->
     <div class="wrap sp-border-b sp-border-slate-200">
-      <p class="sp-min-w-[40px] sp-text-gray-placeholder sp-font-bold">進階</p>
+      <p
+        class="sp-min-w-[36px] sp-text-body-sm sp-text-gray-placeholder sp-font-bold"
+      >
+        進階
+      </p>
       <div class="sp-flex-auto">
         <div class="d-flex pb-3">
-          <v-select
-            v-model="county"
+          <v-autocomplete
+            v-model="filterData.county"
             :items="countyItems"
             item-title="city"
             item-value="city"
             label="縣市"
-            density="comfortable"
+            density="compact"
             single-line
-            class="px-2"
+            class="px-1"
             hide-details
-          ></v-select>
-          <v-select
+            :menu-props="{ location: 'bottom' }"
+          >
+            <template #label>
+              <span class="sp-text-body-sm"> 縣市 </span>
+            </template>
+          </v-autocomplete>
+          <v-autocomplete
+            v-model="filterData.town"
             :items="townItems"
             label="地區"
-            density="comfortable"
+            density="compact"
             single-line
-            item-title="dist"
-            item-value="dist"
-            class="px-2"
+            class="px-1"
             hide-details
-          ></v-select>
+            :disabled="!filterData.county"
+          >
+            <template #label>
+              <span class="sp-text-body-sm"> 地區 </span>
+            </template>
+          </v-autocomplete>
         </div>
-        <v-select
+        <v-autocomplete
+          v-model="filterData.cate"
+          :items="cateItems"
           label="服務類型"
-          density="comfortable"
+          density="compact"
           single-line
           hide-details
-          class="px-2"
-        ></v-select>
+          class="px-1"
+        >
+          <template #label>
+            <span class="sp-text-body-sm"> 服務類型 </span>
+          </template>
+        </v-autocomplete>
       </div>
     </div>
     <!-- 急件 -->
     <div class="wrap sp-items-center sp-border-b sp-border-slate-200">
-      <p class="sp-min-w-[40px] sp-text-gray-placeholder sp-font-bold">急件</p>
+      <p
+        class="sp-min-w-[36px] sp-text-body-sm sp-text-gray-placeholder sp-font-bold"
+      >
+        急件
+      </p>
       <v-checkbox
-        v-model="urgent"
-        label="全部"
+        v-model="filterData.urgent"
         hide-details
         density="comfortable"
-      ></v-checkbox>
+      >
+        <template #label>
+          <span class="sp-text-body-sm"> 全部 </span>
+        </template>
+      </v-checkbox>
     </div>
     <!-- actions -->
     <div class="wrap">
-      <v-btn class="ml-auto" variant="plain">全部重設</v-btn>
+      <v-btn @click="resetFilter" class="ml-auto" variant="plain"
+        >全部重設</v-btn
+      >
       <v-btn color="v-purple" rounded="lg">套用</v-btn>
     </div>
   </div>
@@ -78,12 +121,52 @@
 import { FunnelIcon } from "@heroicons/vue/24/outline";
 import countyData from "@/static/tw_county.json";
 import townData from "@/static/tw_town.json";
-const countyItems = computed(() => countyData);
-const townItems = computed(() => townData);
+import { getCategories } from "@/services/apis/general";
 
-const sort = ref("latest");
-const county = ref(null);
-const urgent = ref(false);
+// 進階篩選資料
+const countyItems = computed(() => countyData);
+const townItems = ref([]);
+const cateItems = ref([]);
+const filterData = reactive({
+  sort: "latest",
+  county: null,
+  town: null,
+  cate: null,
+  urgent: false,
+});
+watch(
+  () => filterData.county,
+  (val) => {
+    filterData.town = null;
+    townItems.value = townData.reduce((acc, cur) => {
+      if (cur.city == val) {
+        acc.push(cur.dist);
+      }
+      return acc;
+    }, []);
+  }
+);
+const fetchCategories = async () => {
+  let res = await getCategories();
+  console.log({ res });
+  cateItems.value = res.data.reduce((acc, cur) => {
+    acc.push(cur.name);
+    return acc;
+  }, []);
+};
+onMounted(() => {
+  fetchCategories();
+});
+// reset
+const resetFilter = () => {
+  Object.assign(filterData, {
+    sort: "latest",
+    county: null,
+    town: null,
+    cate: null,
+    urgent: false,
+  });
+};
 </script>
 
 <style lang="postcss" scoped>
@@ -91,6 +174,14 @@ const urgent = ref(false);
   @apply sp-bg-white sp-rounded-2xl;
 }
 .wrap {
-  @apply sp-flex sp-px-4 sp-py-3;
+  @apply sp-flex sp-p-3;
+}
+.v-autocomplete {
+  &:deep(.v-autocomplete__selection) {
+    @apply sp-text-body-sm sp-leading-6;
+  }
+  &:deep(input) {
+    @apply sp-text-body-sm sp-leading-6;
+  }
 }
 </style>
