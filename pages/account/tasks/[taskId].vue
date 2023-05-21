ca78e2bfb5a3cd2bec100a5d311ded213eaef8be
qw050123 <qw050123@gmail.com>
2023-05-21 11:16:12 +0800

    Add: progress

--- pages/account/tasks/[taskId].vue
+++ pages/account/tasks/[taskId].vue
@@ -1,7 +1,30 @@
 <template>
-  <div>您好</div>
+  <div>
+    <VContainer fluid>
+      <VRow>
+        <VCol>
+          <AccountTasksProgress></AccountTasksProgress>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import { getTasksManagementDetail } from "@/services/apis/tasks";
+  const route = useRoute();
+  const taskId = route.params.taskId;
+  const detail = useState("taskDetail", () => ref());
+  const FuncGetTasksManagementDetail = async () => {
+    let res = await getTasksManagementDetail(taskId);
+    if (!res.error) {
+      detail.value = res.data;
+    } else {
+    }
+  };
+  onMounted(() => {
+    FuncGetTasksManagementDetail();
+  });
+</script>
 
 <style lang="scss" scoped></style>
