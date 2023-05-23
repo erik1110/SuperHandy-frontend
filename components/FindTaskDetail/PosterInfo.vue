bbb5a5e5b445153f7bb52f48806e782f4ea573a9
AnnannannA <83194360+annaliu2021@users.noreply.github.com>
2023-05-23 10:53:01 +0800

    Add: layout

---
+++ components/FindTaskDetail/PosterInfo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="sp-bg-white sp-rounded-2xl sm:sp-mr-6 sp-min-w-[250px] sm:sp-max-w-[300px]">
+        <div class="wrap sp-items-center sp-border-b sp-border-slate-300">
+            <p class="sp-font-bold">案主資訊</p>
+        </div>
+        <div class="wrap">
+            <p class="sp-min-w-[44px] sp-text-body-sm sp-text-gray-placeholder sp-font-bold">
+                姓名
+            </p>
+            <p class="sp-text-body-sm sp-text-gray-placeholder sp-font-bold">
+                姓名
+            </p>
+        </div>
+        <div class="wrap">
+            <p class="sp-min-w-[44px] sp-text-body-sm sp-text-gray-placeholder sp-font-bold">
+                電話
+            </p>
+            <p class="sp-text-body-sm sp-text-gray-placeholder sp-font-bold">
+                電話
+            </p>
+        </div>
+        <div class="wrap sp-border-b sp-border-slate-200">
+            <p class="sp-min-w-[44px] sp-text-body-sm sp-text-gray-placeholder sp-font-bold">
+                Email
+            </p>
+            <p class="sp-min-w-[36px] sp-text-body-sm sp-text-gray-placeholder sp-font-bold">
+                Email
+            </p>
+        </div>
+        <div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="postcss" scoped>
+@import url("@/assets/css/tailwind.css");
+
+.wrap {
+    @apply sp-flex sp-p-3;
+}
+</style>
--- pages/find-task/[taskId].vue
+++ pages/find-task/[taskId].vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    <h1>找任務-查看詳情</h1>
-    taskId:{{ $route.params.taskId }}
+    <div
+      class="sp-max-w-[1680px] sp-mx-auto sp-flex sp-flex-wrap sp-items-center sp-justify-center sm:sp-justify-start lg:sp-justify-around lg:sp-flex-nowrap sp-px-5 sp-pb-12">
+      <FindTasksHighlight />
+    </div>
+    <div class="sp-bg-gray-bg sp-h-full sp-pt-4">
+      <v-container class="sp-flex sp-flex-wrap sm:sp-flex-nowrap sp-content-start">
+        <!-- Filter -->
+        <div class="sp-mb-3 sp-flex-auto sm:sp-flex-initial">
+          <FindTaskDetailPosterInfo />
+        </div>
+        <!-- Main Content -->
+        <div class="sp-flex-auto">
+          <FindTasksTabs />
+          <NuxtPage />
+        </div>
+      </v-container>
+    </div>
   </div>
 </template>
 
-<script setup>
-
-</script>
-
-<style lang="scss" scoped></style>
+<script setup></script>
+<style lang="postcss" scoped></style>
