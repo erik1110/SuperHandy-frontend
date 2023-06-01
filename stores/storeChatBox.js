cc9220539076d744cd6beb4211dea685595bcc40
rere2133 <rere2133@users.noreply.github.com>
2023-06-02 02:35:44 +0800

    Other: turn show chat to false

--- stores/storeChatBox.js
+++ stores/storeChatBox.js
@@ -2,7 +2,7 @@
 import { defineStore } from "pinia";
 
 export const storeChatBox = defineStore("chatBox", () => {
-  const showChat = ref(true)
+  const showChat = ref(false)
   const nowRoom = reactive({})
   const messages = reactive([
     {name: '王小華', msg: "Hello World!"},
